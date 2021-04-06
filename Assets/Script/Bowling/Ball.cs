3f8a8dddd0b7bc5262eb623adc4d63966b60a382
cp3xzayn <cp3xzayn@gmail.com>
2021-04-07 03:48:07 +0900

    簡易的なゴルフみたいなものを作成した。

---
+++ Assets/Script/Bowling/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] float m_zVelocity = 0;
+    [SerializeField] float m_yVelocity = 0;
+    Rigidbody rb;
+    SliderController sliderController;
+    [SerializeField] GameObject m_sliderController = null;
+
+    bool isShot = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        sliderController = m_sliderController.GetComponent<SliderController>();
+    }
+
+
+    public void Shot()
+    {
+        if (isShot)
+        {
+            m_zVelocity = sliderController.ZPower;
+            m_yVelocity = sliderController.YPower;
+            rb.AddForce(0, m_yVelocity, m_zVelocity);
+            isShot = false;
+        }
+    }
+}
---
+++ Assets/Script/Bowling/SliderController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderController : MonoBehaviour
+{
+    [SerializeField] Slider m_zSlider = null;
+    [SerializeField] Slider m_ySlider = null;
+    [SerializeField] GameObject m_ball = null;
+    Ball ballController;
+    bool isZIncrease = false;
+    bool isYIncrease = false;
+    TurnState turnState;
+    private float m_zPower;
+    private float m_yPower;
+
+    public float ZPower
+    {
+        get { return m_zPower * 100; }
+    }
+
+    public float YPower
+    {
+        get { return m_yPower * 100; }
+    }
+
+    void Start()
+    {
+        m_zSlider.GetComponent<Slider>();
+        m_ySlider.GetComponent<Slider>();
+        ballController = m_ball.GetComponent<Ball>();
+    }
+
+    void Update()
+    {
+        switch (turnState)
+        {
+            case TurnState.ZDecide:
+                if (isZIncrease)
+                {
+                    m_zSlider.value += 0.01f;
+                    if (m_zSlider.value == 1)
+                    {
+                        isZIncrease = false;
+                    }
+                }
+                else
+                {
+                    m_zSlider.value -= 0.01f;
+                    if (m_zSlider.value == 0)
+                    {
+                        isZIncrease = true;
+                    }
+                }
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    m_zPower = m_zSlider.value;
+                    turnState = TurnState.YDecide;
+                }
+                break;
+            case TurnState.YDecide:
+                if (isYIncrease)
+                {
+                    m_ySlider.value += 0.01f;
+                    if (m_ySlider.value == 1)
+                    {
+                        isYIncrease = false;
+                    }
+                }
+                else
+                {
+                    m_ySlider.value -= 0.01f;
+                    if (m_ySlider.value == 0)
+                    {
+                        isYIncrease = true;
+                    }
+                }
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    m_yPower = m_ySlider.value;
+                    turnState = TurnState.Decided;
+                }
+                break;
+            case TurnState.Decided:
+                ballController.Shot();
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+public enum TurnState
+{
+    ZDecide,
+    YDecide,
+    Decided
+}
