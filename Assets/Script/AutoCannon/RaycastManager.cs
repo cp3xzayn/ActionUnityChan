b2b1dbde1908e0ea56bf31014c5a6cd5d397d799
cp3xzayn <cp3xzayn@gmail.com>
2021-04-07 04:27:22 +0900

    Rayを飛ばし、マウスカーソルが当たってるところにHitPointを生成するようにした。

---
+++ Assets/Script/AutoCannon/RaycastManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastManager : MonoBehaviour
+{
+    [SerializeField] GameObject m_hitPoint = null;
+    bool isMoveHitPoint = true;
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (isMoveHitPoint)
+            {
+                m_hitPoint.transform.position = hit.point;
+            }
+            if (hit.collider.gameObject.tag == "Field")
+            {
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    isMoveHitPoint = false;
+                }
+            }
+        }
+    }
+}
