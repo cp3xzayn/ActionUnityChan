181a21de21b64cb4369ea3c1491b85dce749066d
cp3xzayn <cp3xzayn@gmail.com>
2021-04-09 20:23:15 +0900

    弾を飛ばす位置を決定し、斜方投射で弾を飛ばせるようにした。

--- Assets/Script/AutoCannon/RaycastManager.cs
+++ Assets/Script/AutoCannon/RaycastManager.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 public class RaycastManager : MonoBehaviour
 {
     [SerializeField] GameObject m_hitPoint = null;
+    /// <summary> 発射位置を移動できるか </summary>
     bool isMoveHitPoint = true;
+
+    /// <summary> ターゲットの位置 </summary>
+    private Vector3 m_targetPos;
+    /// <summary> ターゲットの位置 </summary>
+    public Vector3 TargetPos{ get { return m_targetPos; }　}
+
+    /// <summary> 発射位置が決定されたか </summary>
+    private bool isDecide = false;
+    /// <summary> 発射位置が決定されたか </summary>
+    public bool IsDecide {　 get { return isDecide; }　}
 
     void Update()
     {
@@ -15,15 +26,23 @@ public class RaycastManager : MonoBehaviour
         {
             if (isMoveHitPoint)
             {
-                m_hitPoint.transform.position = hit.point;
+                m_hitPoint.transform.position
+                    = new Vector3(hit.point.x, hit.point.y + 0.01f, hit.point.z);
             }
             if (hit.collider.gameObject.tag == "Field")
             {
                 if (Input.GetButtonDown("Fire1"))
                 {
+                    m_targetPos = hit.point;
+                    isDecide = true;
                     isMoveHitPoint = false;
                 }
             }
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            isDecide = false;
+            isMoveHitPoint = true;
         }
     }
 }
