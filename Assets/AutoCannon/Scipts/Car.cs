3adf1ce981387a7e5c42271035711ab2469e47c3
cp3xzayn <cp3xzayn@gmail.com>
2021-04-11 18:41:43 +0900

    戦車を追加し、戦車の銃口から弾を生成するようにした。

---
+++ Assets/AutoCannon/Scipts/Car.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Car : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos;
+    [SerializeField] float maxMotorTorque = 1000;
+    [SerializeField] float maxSteeringAngle = 40;
+
+    [SerializeField] WheelCollider wheelFL = null;
+    [SerializeField] WheelCollider wheelFR = null;
+    [SerializeField] WheelCollider wheelBL = null;
+    [SerializeField] WheelCollider wheelBR = null;
+
+    [SerializeField] Transform wheelFLTrans = null;
+    [SerializeField] Transform wheelFRTrans = null;
+    [SerializeField] Transform wheelBLTrans = null;
+    [SerializeField] Transform wheelBRTrans = null;
+
+    void Update()
+    {
+        //wheelcolliderの回転速度に合わせてタイヤモデルを回転させる
+        wheelFLTrans.Rotate(wheelFL.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+        wheelFRTrans.Rotate(wheelFR.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+        wheelBLTrans.Rotate(wheelBL.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+        wheelBRTrans.Rotate(wheelBR.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+
+        //wheelcolliderの角度に合わせてタイヤモデルを回転する（フロントのみ）
+        wheelFLTrans.localEulerAngles = new Vector3(wheelFLTrans.localEulerAngles.x, wheelFL.steerAngle - wheelFLTrans.localEulerAngles.z, wheelFLTrans.localEulerAngles.z);
+        wheelFRTrans.localEulerAngles = new Vector3(wheelFRTrans.localEulerAngles.x, wheelFR.steerAngle - wheelFRTrans.localEulerAngles.z, wheelFRTrans.localEulerAngles.z);
+
+    }
+
+    public void FixedUpdate()
+    {
+        float motor = -maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+    }
+}
+
+[Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; //駆動輪か?
+    public bool steering; //ハンドル操作をしたときに角度が変わるか？
+}
